bc50292e6fef590667b4414dbd1e778cf5a62757
Kartones <diego@kartones.net>
2014-05-10 18:53:00 +0200

    .

--- js/temperatures.js
+++ js/temperatures.js
@@ -16,7 +16,7 @@ function getTemperature(id) {
     }
   };
 
-  temperatureRequest.open('GET', 'http://jorgepazj.cartodb.com/api/v2/sql?api_key=5623ba4d3f266753437e57701ffd8b0759f49410&q='
+  temperatureRequest.open('GET', 'http://jorgepazj.cartodb.com/api/v2/sql?q='
     + 'SELECT%20*%20FROM%20temperatures%20WHERE%20internalname=%27'
     + id  
     + '%27'
